03b14e14227a6507a2036f7d4c6ef75e82e43b0c
kiku ishimoto <kikuish@gmail.com>
2020-05-05 19:13:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,14 @@
 katz_deli =[]
-def line(katz_deli)
-  counter = 1
-  if katz_deli == 0
-    puts "The line is currently empty."
-  else
-    katz_deli.each do |people|
-      puts "you are "
+def line(array)
+  if kat_deli >= 1
+    nuarray = []
+    counter = 1
+    array.each do |name|
+      nuarray << "#{counter}. #{name}"
+      counter += 1
     end
+    puts "The line is currently: #{nuarray.join(' ')}"
+  else
+    puts "The line is currently empty."
   end
 end

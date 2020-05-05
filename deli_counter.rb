6a51ef4f75d57ee69a982d74c5d1ca6b269256a8
kiku ishimoto <kikuish@gmail.com>
2020-05-05 19:34:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 def now_serving(line)
   if line.length > 0
     puts "Currently serving #{line.first}."
-    line.pop
+    line.shift
   else
     puts "There is nobody waiting to be served!"
   end

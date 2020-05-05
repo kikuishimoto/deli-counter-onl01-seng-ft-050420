06b8a571ff2e669cb54a77f7c1a680d74abb4eeb
kiku ishimoto <kikuish@gmail.com>
2020-05-05 19:01:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli =[]
+def line(katz_deli)
+  counter = 1
+  if katz_deli == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each do |people|
+      puts "you are "
+    end
+  end
+end

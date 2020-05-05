5628058fc0b89236bd6cc638a21702efd795f789
kiku ishimoto <kikuish@gmail.com>
2020-05-05 19:29:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,11 @@ def take_a_number(line, new_name)
   line.push(new_name)
   puts "Welcome, #{new_name}. You are number #{line.length} in line."
 end
+
+def now_serving(line)
+  if line.length > 0
+    put "#{line.first}, your order is ready"
+    line.pop
+  else
+    puts "there are no more orders"
+end
